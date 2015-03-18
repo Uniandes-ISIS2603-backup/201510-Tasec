3b39edba103a04b0a880bde985e055cf9d9b2dfa
dfSabogal10 <david.sabogal1@gmail.com>
2015-03-18 10:39:12 -0500

    Master organizado

--- FrontEnd/public_html/modules/Servicio/servicio.ctrl.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    var servicioModule = angular.module('servicioModule');
-
-    servicioModule.controller('servicioCtrl', ['$scope', 'servicioservice', function ($scope, servicioservice) {
-            servicioservice.extendCtrl(this, $scope);
-            this.fetchRecords();
-        }]);
-})();
--- FrontEnd/public_html/modules/Servicio/servicio.svc.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    var servicioModule = angular.module('servicioModule');
-    
-    servicioModule.service('servicioservice', ['CRUDBase','servicio.context', function(CRUDBase, context){
-            this.url = context;
-            CRUDBase.extendService(this);
-    }]);
-})();
