5994da4a384ceceb7c1fe1a13fcec7ca49a7a592
Fabio <fa.espinosa10@uniandes.edu.co>
2015-04-08 13:19:20 -0500

    nueva template navbar

---
+++ FrontEnd/public_html/src/app.js
@@ -0,0 +1,60 @@
+ 
+ 
+
+(function(){
+    (function () {
+
+        var mainApp = angular.module('mainApp', 
+        ['ngRoute', 'registrar']);
+
+        //Configuración de navegación entre rutas
+        //Determina qué template cargar cuando el navegador busque una URL específica
+        mainApp.config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'src/modules/home/home.html'
+            }).when('/admin',{
+                templateUrl: 'src/modules/registrar/registrarse.html'
+            }).when('/item',{
+                templateUrl: 'src/modules/item/item.html'
+            }).when('/catalogo',{
+                templateUrl: 'src/modules/catalogo/catalogo.html'
+            }).when('/registrarse',{
+                templateUrl: 'src/modules/registrar/registrarse.html'
+            }).when('/administrarCatalogo',{
+                templateUrl: 'src/modules/administrador/administrarCatalogo.html'
+            }).when('/buscar',{
+                templateUrl:'src/modules/buscar/buscar.html'
+            }).otherwise('/',{
+                redirectTo:'src/modules/home/home.html'
+            });  
+        }]);
+    
+        mainApp.controller('mainController',['$scope', function($scope){
+            
+         
+                       
+            
+            //usuarios
+            //admin, proveedor, cliente
+            $scope.setUsuario=function(usuario){$scope.tipo=usuario;}
+            $scope.usuarioActual= function(actual){
+                return $scope.tipo === actual;
+            }
+            
+            //
+            
+            this.logout= function(){ this.tipo='normal';}
+            this.login=function(nombre){
+                $scope.logged='true';
+                $scope.nombre=nombre;
+            }
+
+        }]);
+            
+        
+    })();
+ 
+
+ 
+ 
+})();
--- FrontEnd/src/modules/servicio/servicio.mod.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var servicioModule = angular.module('servicioModule', ['CrudModule', 'MockModule']);
-
-    servicioModule.constant('servicio.context', 'servicios');
-
-    servicioModule.config(['servicio.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-            urlsProvider.registerUrl(context);
-        }]);
-})();
