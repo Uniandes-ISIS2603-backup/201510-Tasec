8093444189dfd7e541b9fd51b0bc7ed64a336cda
Fabio <fa.espinosa10@uniandes.edu.co>
2015-04-08 13:31:33 -0500

    nuevo

---
+++ public_html/src/modules/registrar/registrarse.ctrl.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function () {
+    var registrar = angular.module('registrar', ['CrudModule', 'MockModule']);
+
+
+    registrar.constant('cliente.context', 'cliente');
+
+    registrar.config(['cliente.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
+            urlsProvider.registerUrl(context);
+        }]);
+
+
+    registrar.controller('registrarCliente', ['$scope', function ($scope) {
+            
+        }]);
+    
+    
+    registrar.service('clienteservice', ['CRUDBase','cliente.context', function(CRUDBase, context){
+            this.url = context;
+            CRUDBase.extendService(this);
+    }]);
+})();
