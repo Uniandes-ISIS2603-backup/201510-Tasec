c3bd97d0b12e026d31004ad80200b80c70d0b350
Fabio <fa.espinosa10@uniandes.edu.co>
2015-04-16 11:23:33 -0500

    front end en back end, mocks, skipmock

---
+++ BackEnd/TASEC.service/src/main/webapp/src/modules/noticias/forms/modificarNoticias.mod.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function(){
+    var adminModule= angular.module('adminModule',['CrudModule', 'MockModule']);
+    
+    adminModule.constant('admin.context', 'admins');
+
+    adminModule.config(['admin.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
+            urlsProvider.registerUrl(context);
+        }]);
+})();
+
